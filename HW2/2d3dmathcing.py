f94ce1328dea7e31990a267e9a1c3e28aba24798
youxin1231 <tommy135721@gmail.com>
2022-10-23 18:04:25 +0800

    Done Q1

--- HW2/2d3dmathcing.py
+++ HW2/2d3dmathcing.py
@@ -8,7 +8,6 @@ import random
 import cv2
 import open3d as o3d
 from tqdm import trange
-random.seed(2022)
 
 images_df = pd.read_pickle("data/images.pkl")
 train_df = pd.read_pickle("data/train.pkl")
@@ -26,7 +25,7 @@ def average_desc(train_df, points3D_df):
     desc = desc.join(points3D_df.set_index("POINT_ID"), on="POINT_ID")
     return desc
 
-def pnpsolver(query,model,cameraMatrix=0,distortion=0):
+def pnpsolver(query, model, cameraMatrix=0, distortion=0):
     kp_query, desc_query = query
     kp_model, desc_model = model
 
@@ -50,19 +49,19 @@ def pnpsolver(query,model,cameraMatrix=0,distortion=0):
     cameraMatrix = np.array([[1868.27,0,540],[0,1869.18,960],[0,0,1]])    
     distCoeffs = np.array([0.0847023,-0.192929,-0.000201144,-0.000725352])
 
-    return Ransac(points3D, points2D, cameraMatrix, distCoeffs)
+    return DLTRansac(points3D, points2D, cameraMatrix, distCoeffs)
 
-def Ransac(points3D, points2D, cameraMatrix, distCoeffs):
+def DLTRansac(points3D, points2D, cameraMatrix, distCoeffs):
     N = 20
     threshold = 15
     k = 6
-
-    num_best_inliners = 0
+    num_best_inliners, best_R, Best_T = 0, None, None
 
     for i in range(N):
         chosen_idx = random.sample(range(len(points3D)), k)
         chosen_points3D = np.array([points3D[i] for i in chosen_idx])
         chosen_points2D = np.array([points2D[i] for i in chosen_idx])
+        
         # R, t = solveP3P(chosen_points3D, chosen_points2D, cameraMatrix, distCoeffs)
         R_, t = solveDLT(chosen_points3D, chosen_points2D, cameraMatrix, distCoeffs)
 
@@ -78,7 +77,9 @@ def Ransac(points3D, points2D, cameraMatrix, distCoeffs):
             num_best_inliners = num_inliers
             best_R = R_
             best_T = t
-    r = R.from_matrix(best_R)
+    
+    if (num_best_inliners != 0):
+        r = R.from_matrix(best_R)
     return r.as_rotvec(), best_T.T, num_best_inliners
 
 def error_computation(p_t, p_t_hat):
@@ -94,15 +95,29 @@ def solveDLT(points3D, points2D, cameraMatrix, distCoeffs):
     for i in range(len(points3D)):
         [X, Y, Z] = points3D[i]
         [u, v] = points2D[i]
-        A.append([X, Y, Z, 1, 0, 0, 0, 0, -u*X, -u*Y, -u*Z, -u])
-        A.append([0, 0, 0, 0, X, Y, Z, 1, -v*X, -v*Y, -v*Z, -v])
-    U, S, VH = svd(A)
-    F1 = np.array([[VH[-1, 0], VH[-1, 1], VH[-1, 2]],
-                   [VH[-1, 4], VH[-1, 5], VH[-1, 6]],
-                   [VH[-1, 8],VH[-1, 9], VH[-1, 10]]])
-    F2 = np.array([[VH[-1, 3]], [VH[-1, 7]], [VH[-1, 11]]])
-    R_ = inv(cameraMatrix).dot(F1)
-    t = inv(cameraMatrix).dot(F2)
+
+        Cx, Cy = cameraMatrix[0, 2], cameraMatrix[1, 2]
+
+        Cx_u = Cx - u
+        Cy_v = Cy - v
+
+        fx, fy = cameraMatrix[0, 0], cameraMatrix[1, 1]
+        
+        A.append([X * fx, Y * fx, Z * fx, fx, 0, 0, 0, 0, X * Cx_u, Y * Cx_u, Z * Cx_u, Cx_u])
+        A.append([0, 0, 0, 0, X * fy, Y * fy, Z * fy, fy, X * Cy_v, Y * Cy_v, Z * Cy_v, Cy_v])
+
+    U, S, VH_tmp = svd(A)
+    R_tmp = np.array([[VH_tmp[-1, 0], VH_tmp[-1, 1], VH_tmp[-1, 2]],
+                      [VH_tmp[-1, 4], VH_tmp[-1, 5], VH_tmp[-1, 6]],
+                      [VH_tmp[-1, 8],VH_tmp[-1, 9], VH_tmp[-1, 10]]])
+    U, S, VH = np.linalg.svd(R_tmp)
+    c = 1 / (np.sum(S) / 3)
+    R_ = U.dot(VH)
+    tmp = c * (X * VH_tmp[-1, 8] + Y * VH_tmp[-1, 9] + Z * VH_tmp[-1, 10] + VH_tmp[-1, 11])
+    if (tmp) < 0:
+        c = -c
+        R_ = -R_
+    t = c * np.array([VH_tmp[-1, 3], VH_tmp[-1, 7], VH_tmp[-1, 11]]).reshape(3,1)
     return R_, t
 
 def solveP3P(points3D, points2D, cameraMatrix, distCoeffs):
@@ -205,6 +220,7 @@ desc_model = np.array(desc_df["DESCRIPTORS"].to_list()).astype(np.float32)
 R_list, T_list = [], []
 R_diff_list, T_diff_list = [], []
 
+# for idx in trange(1, 11):
 for idx in trange(1, point_desc_df['IMAGE_ID'].max()):
     # Load query image
     # fname = ((images_df.loc[images_df["IMAGE_ID"] == idx])["NAME"].values)[0]
@@ -217,73 +233,99 @@ for idx in trange(1, point_desc_df['IMAGE_ID'].max()):
 
     # Find correspondance and solve pnp
     rvec, tvec, inliers = pnpsolver((kp_query, desc_query),(kp_model, desc_model))
-    r = R.from_rotvec(rvec.reshape(1, 3))
-    rotq = r.as_quat()
-    tvec = tvec.reshape(1, 3)
     
-    R_list.append(r.as_matrix()[0])
-    T_list.append(tvec)
+    if (inliers != 0):
+        r = R.from_rotvec(rvec.reshape(1, 3))
+        rotq = r.as_quat()
+        tvec = tvec.reshape(1, 3)
+        
+        R_list.append(r.as_matrix()[0])
+        T_list.append(tvec)
 
-    # Get camera pose groudtruth 
-    ground_truth = images_df.loc[images_df["IMAGE_ID"]==idx]
-    rotq_gt = ground_truth[["QX","QY","QZ","QW"]].values
-    tvec_gt = ground_truth[["TX","TY","TZ"]].values
+        # Get camera pose groudtruth 
+        ground_truth = images_df.loc[images_df["IMAGE_ID"]==idx]
+        rotq_gt = ground_truth[["QX","QY","QZ","QW"]].values
+        tvec_gt = ground_truth[["TX","TY","TZ"]].values
+        
+        temp = np.abs(rotq_gt.dot(rotq.T))
+        if temp < 1:
+            R_diff = 2 * np.arccos(temp)
+        else:
+            R_diff = 2 * np.arccos(1)
+
+        T_diff = distance.euclidean(tvec_gt[0], tvec[0])
+        
+        R_diff_list.append(R_diff)
+        T_diff_list.append(T_diff)
     
-    temp = np.abs(rotq_gt.dot(rotq.T))
-    if temp < 1:
-        R_diff = 2 * np.arccos(temp)
-    else:
-        R_diff = 2 * np.arccos(1)
-
-    T_diff = distance.euclidean(tvec_gt[0], tvec[0])
-
-    R_diff_list.append(R_diff)
-    T_diff_list.append(T_diff)
-
 print("Median of relative rotation angle differences:", np.median(R_diff_list))
 print("Median of translation differences:", np.median(T_diff_list))
 
-R_list, T_list = np.array(R_list), np.array(T_list)
+R_list, T_list = np.array(R_list).reshape(-1, 3, 3), np.array(T_list).reshape(-1, 1, 3)
 M_inv = []
 
 for r, t in zip(R_list, T_list):
-    t = np.array(t).T
-    rt = np.concatenate((r, t), axis=1)
+    rt = np.concatenate((r, t.T), axis=1)
     M = np.concatenate((rt, [[0, 0, 0, 1]]), axis = 0)
     M_inv.append(inv(M))
 
-apex, base = [], []
+all_points = np.empty((0, 4), float)
 for m in M_inv:
-    apex.append(m.dot([0, 0, 0, 1]).reshape((1,4)))
-    base.append(m.dot([0.2, 0.2, 1, 1]).reshape((1,4)))
-    base.append(m.dot([0.2, -0.2, 1, 1]).reshape((1,4)))
-    base.append(m.dot([-0.2, 0.2, 1, 1]).reshape((1,4)))
-    base.append(m.dot([-0.2, -0.2, 1, 1]).reshape((1,4)))
+    all_points = np.append(all_points, m.dot(np.array([0, 0, 0, 1])).reshape((1,4)), axis=0)
+    all_points = np.append(all_points, m.dot(np.array([0.2, 0.2, 1, 1])).reshape((1,4)), axis=0)
+    all_points = np.append(all_points, m.dot(np.array([-0.2, 0.2, 1, 1])).reshape((1,4)), axis=0)
+    all_points = np.append(all_points, m.dot(np.array([-0.2, -0.2, 1, 1])).reshape((1,4)), axis=0)
+    all_points = np.append(all_points, m.dot(np.array([0.2, -0.2, 1, 1])).reshape((1,4)), axis=0)
 
-lines, lines_color = [], []
-for i in range(len(apex)-2):
-    lines.append([i, i+1])
-    lines_color.append([0, 1, 0])
-apex, lines, lines_color = np.array(apex).reshape(-1, 4), np.array(lines).reshape(-1, 2), np.array(lines_color).reshape(-1, 3)
+lines = np.empty((0, 2), np.int32)
+lines_color = np.empty((0, 3), float)
+triangles = np.empty((0, 3), float)
+
+for i in range(0, all_points.shape[0], 5):
+    if i != (all_points.shape[0] - 5):
+        lines = np.append(lines, np.array([[i, i + 5]]), axis=0)
+        lines_color = np.append(lines_color, np.array([[0, 1, 0]]), axis=0)
+
+    lines = np.append(lines, np.array([[i + 1, i + 2]]), axis=0)
+    lines_color = np.append(lines_color, np.array([[0, 0, 0]]), axis=0)
+
+    lines = np.append(lines, np.array([[i + 2, i + 3]]), axis=0)
+    lines_color = np.append(lines_color, np.array([[0, 0, 0]]), axis=0)
+
+    lines = np.append(lines, np.array([[i + 3, i + 4]]), axis=0)
+    lines_color = np.append(lines_color, np.array([[0, 0, 0]]), axis=0)
+
+    lines = np.append(lines, np.array([[i + 4, i + 1]]), axis=0)
+    lines_color = np.append(lines_color, np.array([[0, 0, 0]]), axis=0)
+
+    lines = np.append(lines, np.array([[i, i + 1]]), axis=0)
+    lines_color = np.append(lines_color, np.array([[0, 0, 0]]), axis=0)
+
+    lines = np.append(lines, np.array([[i, i + 2]]), axis=0)
+    lines_color = np.append(lines_color, np.array([[0, 0, 0]]), axis=0)
+
+    lines = np.append(lines, np.array([[i, i + 3]]), axis=0)
+    lines_color = np.append(lines_color, np.array([[0, 0, 0]]), axis=0)
+
+    lines = np.append(lines, np.array([[i, i + 4]]), axis=0)
+    lines_color = np.append(lines_color, np.array([[0, 0, 0]]), axis=0)
+
+    triangles = np.append(triangles, np.array([[i + 3, i + 2, i + 1]]), axis=0)
+    triangles = np.append(triangles, np.array([[i + 1, i + 4, i + 3]]), axis=0)
 
 line_set = o3d.geometry.LineSet()
-line_set.points = o3d.utility.Vector3dVector(apex[:, :-1])
+line_set.points = o3d.utility.Vector3dVector(all_points[:, :-1])
 line_set.lines = o3d.utility.Vector2iVector(lines)
 line_set.colors = o3d.utility.Vector3dVector(lines_color)
 
-triangles = []
-for i in range(0, len(base), 4):
-    triangles.append([i, i+1, i+2])
-    triangles.append([i+2, i+3, i])
-
-base, triangles = np.array(base).reshape(-1, 4), np.array(triangles).reshape(-1, 3)
 mesh = o3d.geometry.TriangleMesh()
-mesh.vertices = o3d.utility.Vector3dVector(base[:,:-1])
+mesh.vertices = o3d.utility.Vector3dVector(all_points[:, :-1])
 mesh.triangles = o3d.utility.Vector3iVector(triangles)
-mesh.paint_uniform_color([0, 0, 1])
+mesh.paint_uniform_color([0, 0, 0.5])
 
 points3D_arr = np.array(points3D_df["XYZ"].to_list())
 points3D_RGB_arr = np.array(points3D_df["RGB"].to_list()) / 255
+
 point_cloud = o3d.geometry.PointCloud()
 point_cloud.points = o3d.utility.Vector3dVector(points3D_arr)
 point_cloud.colors = o3d.utility.Vector3dVector(points3D_RGB_arr)
