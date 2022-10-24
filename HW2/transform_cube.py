5fe8e4b71bf63671790d1acee04f0debd8016bb5
youxin1231 <tommy135721@gmail.com>
2022-10-24 20:02:20 +0800

    pass function test

--- HW2/transform_cube.py
+++ HW2/transform_cube.py
@@ -1,9 +1,22 @@
 import open3d as o3d
 import cv2 as cv
-import numpy as np
-from scipy.spatial.transform import Rotation as R
-import sys, os
 import pandas as pd
+
+import numpy as np
+from numpy.polynomial.polynomial import polycompanion
+from numpy.linalg import eig, norm, svd, inv
+
+from scipy.spatial import distance
+from scipy.spatial.transform import Rotation as R
+
+import sys, os
+from tqdm import tqdm
+import random
+
+images_df = pd.read_pickle("data/images.pkl")
+train_df = pd.read_pickle("data/train.pkl")
+points3D_df = pd.read_pickle("data/points3D.pkl")
+point_desc_df = pd.read_pickle("data/point_desc.pkl")
 
 def load_point_cloud(points3D_df):
 
@@ -87,9 +100,158 @@ def update_scale(vis):
     scale += -0.05 if shift_pressed else 0.05
     update_cube()
 
-# if len(sys.argv) != 2:
-#     print('[Usage] python3 transform_cube.py /PATH/TO/points3D.txt')
-#     sys.exit(1)
+
+def average(x):
+    return list(np.mean(x,axis=0))
+
+def average_desc(train_df, points3D_df):
+    train_df = train_df[["POINT_ID","XYZ","RGB","DESCRIPTORS"]]
+    desc = train_df.groupby("POINT_ID")["DESCRIPTORS"].apply(np.vstack)
+    desc = desc.apply(average)
+    desc = desc.reset_index()
+    desc = desc.join(points3D_df.set_index("POINT_ID"), on="POINT_ID")
+    return desc
+
+def pnpsolver(query, model, cameraMatrix=0, distortion=0):
+    kp_query, desc_query = query
+    kp_model, desc_model = model
+
+    bf = cv.BFMatcher()
+    matches = bf.knnMatch(desc_query,desc_model,k=2)
+
+    gmatches = []
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            gmatches.append(m)
+
+    points2D = np.empty((0,2))
+    points3D = np.empty((0,3))
+
+    for mat in gmatches:
+        query_idx = mat.queryIdx
+        model_idx = mat.trainIdx
+        points2D = np.vstack((points2D,kp_query[query_idx]))
+        points3D = np.vstack((points3D,kp_model[model_idx]))
+
+    cameraMatrix = np.array([[1868.27,0,540],[0,1869.18,960],[0,0,1]])    
+    distCoeffs = np.array([0.0847023,-0.192929,-0.000201144,-0.000725352])
+
+    return DLTRansac(points3D, points2D, cameraMatrix, distCoeffs)
+
+def DLTRansac(points3D, points2D, cameraMatrix, distCoeffs):
+    N = 20
+    threshold = 15
+    k = 6
+    num_best_inliners, best_R, best_T = 0, None, None
+
+    for i in range(N):
+        chosen_idx = random.sample(range(len(points3D)), k)
+        chosen_points3D = np.array([points3D[i] for i in chosen_idx])
+        chosen_points2D = np.array([points2D[i] for i in chosen_idx])
+        
+        # R, t = solveP3P(chosen_points3D, chosen_points2D, cameraMatrix, distCoeffs)
+        R_, t = solveDLT(chosen_points3D, chosen_points2D, cameraMatrix, distCoeffs)
+
+        M = cameraMatrix.dot(np.concatenate((R_, t.T), axis=1))
+        u_homo = M.dot(np.concatenate((points3D.T, np.ones((1, points3D.shape[0])))))
+        u = (u_homo / u_homo[2, :])[:2].T
+        error_vector, error = error_computation(points2D, u)
+
+        idx = np.where(error_vector < threshold)[0]
+        num_inliers = len(idx)
+        
+        if num_inliers > num_best_inliners:
+            num_best_inliners = num_inliers
+            best_R = R_
+            best_T = t
+    
+    if (num_best_inliners != 0):
+        r = R.from_matrix(best_R)
+        return r.as_rotvec(), best_T.T, num_best_inliners
+    else:
+        return best_R, best_T, num_best_inliners
+
+def error_computation(p_t, p_t_hat):
+    diff = (p_t - p_t_hat)**2
+    diff = diff.sum(axis = 1)
+    error_vector = np.sqrt(diff)
+    error = error_vector.sum()/p_t.shape[0]
+
+    return error_vector, error
+
+def solveDLT(points3D, points2D, cameraMatrix, distCoeffs):
+    A = []
+    for i in range(len(points3D)):
+        [X, Y, Z] = points3D[i]
+        [u, v] = points2D[i]
+
+        Cx, Cy = cameraMatrix[0, 2], cameraMatrix[1, 2]
+
+        Cx_u = Cx - u
+        Cy_v = Cy - v
+
+        fx, fy = cameraMatrix[0, 0], cameraMatrix[1, 1]
+        
+        A.append([X * fx, Y * fx, Z * fx, fx, 0, 0, 0, 0, X * Cx_u, Y * Cx_u, Z * Cx_u, Cx_u])
+        A.append([0, 0, 0, 0, X * fy, Y * fy, Z * fy, fy, X * Cy_v, Y * Cy_v, Z * Cy_v, Cy_v])
+
+    U, S, VH_tmp = svd(A)
+    R_tmp = np.array([[VH_tmp[-1, 0], VH_tmp[-1, 1], VH_tmp[-1, 2]],
+                      [VH_tmp[-1, 4], VH_tmp[-1, 5], VH_tmp[-1, 6]],
+                      [VH_tmp[-1, 8],VH_tmp[-1, 9], VH_tmp[-1, 10]]])
+    U, S, VH = np.linalg.svd(R_tmp)
+    c = 1 / (np.sum(S) / 3)
+    R_ = U.dot(VH)
+    tmp = c * (X * VH_tmp[-1, 8] + Y * VH_tmp[-1, 9] + Z * VH_tmp[-1, 10] + VH_tmp[-1, 11])
+    if (tmp) < 0:
+        c = -c
+        R_ = -R_
+    t = c * np.array([VH_tmp[-1, 3], VH_tmp[-1, 7], VH_tmp[-1, 11]]).reshape(1,3)
+    return R_, t
+
+def Get_Image_R_T():
+    # Process model descriptors
+    desc_df = average_desc(train_df, points3D_df)
+    kp_model = np.array(desc_df["XYZ"].to_list())
+    desc_model = np.array(desc_df["DESCRIPTORS"].to_list()).astype(np.float32)
+
+    global images_df
+    
+    # Preprocess images_df
+    images_df = images_df[images_df.NAME.str.startswith("valid")]
+    new_id_list = []
+    for name in images_df.NAME:
+        new_name = name[9:]
+        new_name = new_name[:-4]
+        new_id_list.append(int(new_name))
+    images_df['NEW_ID'] = new_id_list
+    images_df.sort_values(by = ["NEW_ID"], inplace = True)
+
+    R_list, T_list, img_list = [], [], []
+
+    for idx in tqdm(images_df.IMAGE_ID):
+        # Load query image
+        fname = ((images_df.loc[images_df["IMAGE_ID"] == idx])["NAME"].values)[0]
+        rimg = cv.imread("data/frames/"+fname)
+        img_list.append(rimg)
+
+        # Load query keypoints and descriptors
+        points = point_desc_df.loc[point_desc_df["IMAGE_ID"]==idx]
+        kp_query = np.array(points["XY"].to_list())
+        desc_query = np.array(points["DESCRIPTORS"].to_list()).astype(np.float32)
+
+        # Find correspondance and solve pnp
+        rvec, tvec, inliers = pnpsolver((kp_query, desc_query),(kp_model, desc_model))
+        
+        if (inliers != 0):
+            r = R.from_rotvec(rvec.reshape(1, 3))
+            tvec = tvec.reshape(1, 3)
+            
+            R_list.append(r.as_matrix()[0])
+            T_list.append(tvec)
+
+    img_list, R_list, T_list = np.array(img_list), np.array(R_list).reshape(-1, 3, 3), np
+    return img_list, R_list, T_list
 
 vis = o3d.visualization.VisualizerWithKeyCallback()
 vis.create_window()
@@ -154,3 +316,53 @@ print('Scale factor: {}'.format(scale))
 
 np.save('cube_transform_mat.npy', get_transform_mat(R_euler, t, scale))
 np.save('cube_vertices.npy', np.asarray(cube.vertices))
+
+
+def main():
+    all_points, point_colors = np.empty((0, 3), float), np.empty((0, 3), float)
+
+    # n points on one side
+    n = 10
+
+    cube_corner = np.load('cube_vertices.npy')
+
+    origin, diagonal = cube_corner[0, :], cube_corner[7, :]
+    all_points = np.append(all_points, np.array(origin).reshape(1,3), axis = 0)
+    point_colors = np.append(point_colors, np.array([0, 0, 255]).reshape(1,3), axis = 0)
+
+    axis1, axis2, axis3 = cube_corner[1, :] - origin, cube_corner[2, :] - origin, cube_corner[4, :] - origin
+
+    origin_planes = [[axis1, axis2], [axis1, axis3], [axis2, axis3]]
+    origin_colors = [[255, 0, 0], [0, 255, 0], [0, 0, 255]]
+    diagonal_planes = [[-axis1, -axis2], [-axis1, -axis3], [-axis2, -axis3]]
+    diagonal_colors = [[255, 255, 0], [255, 0, 255], [0, 255, 255]]
+
+    ori = [origin, origin_planes, origin_colors]
+    dia = [diagonal, diagonal_planes, diagonal_colors]
+
+    # Interpolation points
+    for base, planes, colors in [ori, dia]:
+        for [axis, axis_], color in zip(planes, colors):
+            for x in range(n):
+                for y in range(n):
+                    all_points = np.append(all_points, (base + axis * (x/n) + axis_ * (y/n)).reshape(1,3), axis = 0)
+                    point_colors = np.append(point_colors, np.array(color).reshape(1,3), axis = 0)
+
+    K = np.array([[1868.27,0,540],[0,1869.18,960],[0,0,1]])
+    img_list, R_list, T_list = Get_Image_R_T()
+
+    videowrt = cv.VideoWriter("AR_video.mp4", cv.VideoWriter_fourcc(*'MP4V'), 20, (1080, 1920))
+
+    for img, r, t in zip(img_list, R_list, T_list):
+        M = K.dot(np.concatenate((r, t.T), axis=1))
+
+        for point, color in zip(all_points, point_colors):
+            point2D = M.dot(np.append(point, 1))[:-1] / point2D[-1]
+            point2D = np.int_(point2D)
+            if point2D[0] >= 0 and point2D[0] < 1080 and point2D[1] >= 0 and point2D[1] < 1920:
+                img = cv.circle(img, point2D, 5, color, thickness = -1)
+        videowrt.write(img)
+    videowrt.release()
+
+if __name__ == '__main__':
+    main()
